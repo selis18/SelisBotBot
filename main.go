b5be90597f03c8662a9bcb86b51801ac370aa548
selis <forselis18@gmail.com>
2024-08-07 16:03:59 +0500

    added randomize sprays

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/go-telegram/bot/models"
 	"github.com/joho/godotenv"
 	"github.com/selis18/agents"
+	"github.com/selis18/sprays"
 )
 
 func main() {
@@ -27,6 +28,7 @@ func main() {
 	}
 
 	b.RegisterHandler(bot.HandlerTypeMessageText, "/randa", bot.MatchTypeContains, randAgentHandler)
+	b.RegisterHandler(bot.HandlerTypeMessageText, "/rands", bot.MatchTypeContains, randSprayHandler)
 
 	fmt.Println("Bot started")
 	b.Start(context.Background())
@@ -40,11 +42,45 @@ func randAgentHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
 	}
 
 	randAgent := agents.GetRandomAgent(arrAgents)
-	chatId := os.Getenv("CHATID")
 
-	b.SendMessage(ctx, &bot.SendMessageParams{
-		ChatID:    chatId,
-		Text:      "Самый лучший на " + randAgent.Name,
+	photo := randAgent.Icon
+
+	// Параметры для отправки сообщения с изображением и текстом
+	params := &bot.SendPhotoParams{
+		ChatID:    update.Message.Chat.ID,
+		Photo:     &models.InputFileString{Data: photo},
+		Caption:   "Самый лучший на " + randAgent.Name,
 		ParseMode: models.ParseModeMarkdown,
-	})
+	}
+
+	// Отправка сообщения с изображением
+	_, err = b.SendPhoto(ctx, params)
+	if err != nil {
+		log.Fatalf("Error sending photo: %v", err)
+	}
+}
+
+func randSprayHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	arrSprays, err := sprays.GetSprays()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	randSpray := sprays.GetRandomSpray(arrSprays)
+
+	photo := randSpray.Icon
+
+	// Параметры для отправки сообщения с изображением и текстом
+	params := &bot.SendPhotoParams{
+		ChatID:    update.Message.Chat.ID,
+		Photo:     &models.InputFileString{Data: photo},
+		Caption:   randSpray.Name,
+		ParseMode: models.ParseModeMarkdown,
+	}
+
+	// Отправка сообщения с изображением
+	_, err = b.SendPhoto(ctx, params)
+	if err != nil {
+		log.Fatalf("Error sending photo: %v", err)
+	}
 }
